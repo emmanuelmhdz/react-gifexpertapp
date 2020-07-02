d0cdefbe950f974c9de375cdd0f6183439cecf64
Emmanuel Morales Hernández <emoraleshe@elektra.com.mx>
2020-07-02 12:16:32 -0500

    Se agrega directorio docs

---
+++ docs/precache-manifest.9bced87679bbb3d8478c7c56244c9261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25621c04413f626503eb1d2271e5aa6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ef12305adbf3c3f76b3",
+    "url": "/static/css/main.27e151ad.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef12305adbf3c3f76b3",
+    "url": "/static/js/main.bf0b9868.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
